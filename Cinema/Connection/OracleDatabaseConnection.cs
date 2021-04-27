3ebe8a156fc690787c154a1ec0f96561e439fa1c
alesyademidkevich01 <alesya.demidkevich@mail.ru>
2021-04-27 15:17:59 +0300

    Добавьте файлы проекта.

---
+++ Cinema/Connection/OracleDatabaseConnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema.Connection
+{
+    public static class OracleDatabaseConnection
+    {
+        public static string connection = "Data Source = " +
+                                          "(DESCRIPTION = " +
+                                          "(ADDRESS_LIST =  " +
+                                          "(ADDRESS = " +
+                                          "(PROTOCOL = TCP)" +
+                                          "(HOST = 192.168.56.105)" +
+                                          "(PORT = 1521)))" +
+                                          "(CONNECT_DATA = " +
+                                          "(SERVER = DEDICATED)" +
+                                          "(SERVICE_NAME = CINEMA.be.by)));" +
+                                          "User Id=admin;Password=Alesya2001;";
+
+    }
+}
---
+++ Cinema/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Oracle.ManagedDataAccess.Client;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Cinema.Connection;
+using Cinema.Users;
+using Cinema.Admin;
+
+namespace Cinema
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    
+    public partial class MainWindow : Window
+    {
+        public OracleConnection oracleConnection;
+        public CurrentEmployee currentEmployee;
+        public string currUserPos = "Администратор";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            currentEmployee = new CurrentEmployee();
+            try
+            {
+                using (OracleConnection oracleConnection = new OracleConnection(OracleDatabaseConnection.connection))
+                {
+                    oracleConnection.Open();
+               
+                    OracleParameter login = new OracleParameter
+                    {
+                        ParameterName = "in_login",
+                        Direction = ParameterDirection.Input,
+                        OracleDbType = OracleDbType.Varchar2,
+                        Value = loginText.Text
+                    };
+                    OracleParameter password = new OracleParameter
+                    {
+                        ParameterName = "in_password",
+                        Direction = ParameterDirection.Input,
+                        OracleDbType = OracleDbType.Varchar2,
+                        Value = passwordText.Password
+                    };
+                    OracleParameter user = new OracleParameter
+                    {
+                        ParameterName = "user_cur",
+                        Direction = ParameterDirection.Output,
+                        OracleDbType = OracleDbType.RefCursor
+                    };
+
+                    using (OracleCommand command = new OracleCommand("findUser"))
+                    {
+                        command.Connection = oracleConnection;
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddRange(new OracleParameter[] { login, password, user });
+                        var reader = command.ExecuteReader();
+                        DataTable dt = new DataTable();
+                        dt.Load(reader);
+
+                        foreach (DataRow row in dt.Rows)
+                        {                            
+                            currentEmployee.login = row["Login"].ToString();
+                            currentEmployee.password = row["Password"].ToString();
+                            currentEmployee.IDEmployee = row["IDEmployee"].ToString();
+                        }
+                    }
+
+                    oracleConnection.Close();
+                    currUserPos = loginText.Text;
+                    if (currUserPos == currentEmployee.login)
+                    {
+                        AdminWindow adminWindow = new AdminWindow();
+                        adminWindow.Show();
+                        this.Close();
+                    }
+                }
+            }
+            catch (OracleException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+    }
+}
